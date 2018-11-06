3150a7c42407f1a8ef3bd2050ce79ee8c096fb3b
kinziemac <mackenziehiga@Mackenzies-MBP.hitronhub.home>
2018-11-06 12:04:35 -0800

    partially finished UI for a start search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import time
 import threading
+import sys
 
 from index import Index
 from search_inserts import aStarSortedInsert, bestFirstInsert, inExpandedList
@@ -9,55 +10,28 @@ from parse_data import *
 from index_checking import *
 from create_path import *
 
-# class Board():
-#     def __init__(self):
-#         self.thread = threading.Thread(target = self.makeMap)
-#         self.thread.start()
-#
-#     def makeMap(self):
-#
-#
-#     def callback(self):
-#         self.root.quit()
-#
-#     def updateSquare(self):
-#         tk.Label(self.root, text=str(map[i][j]), background=colors[(map[i][j] - 1)], borderwidth=15 ).grid(row=i,column=j)
-#         self.makeMap()
-#
-#
-#
 
+def changeTileColor(row, column, color, sleepTime=1000.0):
+    time.sleep(1/sleepTime)
+    tk.Label(root, text=str(map[row][column]), background=color, borderwidth=3 ).grid(row=row,column=column)
 
+def closeThread():
+    time.sleep(4)
+    root.quit()
+    sys.exit()
 
+def writePath(expandedList):
+    pathList = []
+    createPath(pathList, expandedList)
+    for element in pathList:
+        changeTileColor(element.y, element.x, '#FFD700', 250.0)
 
+def aStarSearch():
+    noSolution = False
 
+    if invalidInitials(startIndex, boundary) or invalidInitials(goalIndex, boundary):
+        noSolution = True
 
-txtValues = initializeTxtData()
-boundary = txtValues["boundary"]
-startIndex = txtValues["start"]
-goalIndex = txtValues["goal"]
-map = txtValues["map"]
-printer = Printer()
-searchOutput = []
-noSolution = False
-searchCounter = 0
-
-# board = Board()
-# time.sleep(2)
-# updateSquare(4,4)
-root = tk.Tk()
-colors = ['#e6f7ff', '#b3e6ff', '#80d4ff', '#33bbff', '#00aaff', '#0077b3']
-# root.protocol("WM_DELETE_WINDOW", callback)
-
-for i in range(boundary):
-    for j in range(boundary):
-        tk.Label(root, text=str(map[i][j]), background=colors[(map[i][j] - 1)], borderwidth=15 ).grid(row=i,column=j)
-root.mainloop()
-
-if invalidInitials(startIndex, boundary) or invalidInitials(goalIndex, boundary):
-    noSolution = True
-
-while searchCounter < 2 and noSolution == False:
     openQueue = []
     expandedStack = []
     path = []
@@ -68,9 +42,13 @@ while searchCounter < 2 and noSolution == False:
     atGoal = False
 
     while (atGoal == False and noSolution == False):
+        changeTileColor(presentIndex.y, presentIndex.x, 'orange')
         if presentIndex.distance == 0:
             atGoal = True
+            changeTileColor(presentIndex.y, presentIndex.x, '#00FA9A')
             expandedStack.append(presentIndex)
+            writePath(expandedStack)
+            closeThread()
         else:
             movablePlaces = [
                 Index(
@@ -102,45 +80,47 @@ while searchCounter < 2 and noSolution == False:
             mapRemoval = removeInvalidMoves(map, boundary, presentIndex, expandedStack)
             filteredMoves = filter(mapRemoval, movablePlaces)
             for element in filteredMoves:
-                if searchCounter == 0:
-                    bestFirstInsert(element, openQueue)
-                else:
-                    aStarSortedInsert(element, openQueue)
+                # if searchCounter == 0:
+                #     bestFirstInsert(element, openQueue)
+                # else:
+                aStarSortedInsert(element, openQueue)
 
             openQueue.remove(presentIndex)
             expandedStack.append(presentIndex)
             if isListEmpty(openQueue):
                 noSolution = True
+                closeThread()
+
             else:
                 presentIndex = openQueue[0]
 
-    if noSolution == False:
-        createPath(path, expandedStack)
-        searchOutput.append(len(expandedStack))
-        searchOutput.append(expandedStack[-1].pathCost)
-        searchOutput.append(path)
-        atGoal = False
-
-    searchCounter += 1
-
-print('finished')
+        if noSolution == False:
+            createPath(path, expandedStack)
+            searchOutput.append(len(expandedStack))
+            searchOutput.append(expandedStack[-1].pathCost)
+            searchOutput.append(path)
+            atGoal = False
 
 
-# if noSolution == True:
-#     print ("No Solution")
-# else:
-#     printer.printOutput(searchOutput)
+# Main #
 
 
-# map counts as global variable
-# map = [[1, 1, 1, 2, 1, 1, 1, 1, 1, 1], #0
-#        [1, 2, 2, 2, 1, 1, 1, 1, 1, 1], #1
-#        [1, 2, 3, 2, 1, 3, 3, 1, 1, 1], #2
-#        [4, 2, 2, 2, 2, 1, 1, 1, 1, 6], #3
-#        [1, 2, 1, 1, 1, 1, 1, 1, 6, 1], #4
-#        [1, 1, 1, 1, 1, 6, 1, 6, 1, 1], #5
-#        [1, 1, 3, 1, 1, 1, 6, 1, 1, 1], #6
-#        [1, 3, 1, 1, 6, 1, 1, 1, 2, 1], #7
-#        [1, 1, 1, 1, 1, 1, 6, 1, 2, 1], #8
-#        [1, 1, 1, 1, 1, 6, 1, 1, 1, 1]] #9
-#         0  1  2  3  4  5  6  7  8  9
+txtValues = initializeTxtData()
+boundary = txtValues["boundary"]
+startIndex = txtValues["start"]
+goalIndex = txtValues["goal"]
+map = txtValues["map"]
+printer = Printer()
+searchOutput = []
+searchCounter = 0
+
+root = tk.Tk()
+colors = ['#BBFFFF', '#b3e6ff', '#80d4ff', '#33bbff', '#00aaff', '#0077b3']
+
+for i in range(boundary):
+    for j in range(boundary):
+        tk.Label(root, text=str(map[i][j]), background=colors[(map[i][j] - 1)], borderwidth=3 ).grid(row=i,column=j)
+
+t1 = threading.Thread(target=aStarSearch)
+t1.start()
+root.mainloop()
